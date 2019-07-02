df68cef938f66b9a09dc25f243f4ecbc817197d9
gal kahana <gal.kahana@hotmail.com>
2019-07-02 10:44:08 +0300

    explanations about what the app container does

--- example/plugins-mgmt/plugin-container.js
+++ example/plugins-mgmt/plugin-container.js
@@ -6,6 +6,18 @@ const IsolatedAppNavigationOptions = {
     header: null
 }
 
+/**
+ * The app container takes care of the following:
+ * 1. Isolate any possible "screen props" from the plugin app. Done by wrapping
+ *    the plugin app with something that doesn't pass through props
+ * 2. Isolate app store from the plugin app. Done by using a provider which sets
+ *    the store to be a null store (just empty one).
+ * 3. Nulls navigation options, so won't show header or something
+ * 4. pass navigation params as "parameters" props as part of the container app implementation
+ *    method of passing parameters to apps.
+ * @param {*} screenImporter 
+ */
+
 function PluginContainer(screenImporter) {
     const TheComponentGlobal = screenImporter()
     const wrapper = class extends React.PureComponent {
@@ -27,20 +39,3 @@ function PluginContainer(screenImporter) {
 }
 
 export default PluginContainer
-
-/**
- * const initialState = {
-  visibilityFilter: VisibilityFilters.SHOW_ALL,
-  todos: []
-}
-
-function todoApp(state, action) {
-  if (typeof state === 'undefined') {
-    return initialState
-  }
-
-  // For now, don't handle any actions
-  // and just return the state given to us.
-  return state
-}
- */
