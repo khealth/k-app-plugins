59705ad418f88e57b8c4caf110707391d4aca9bc
gal kahana <gal.kahana@hotmail.com>
2019-07-07 16:50:37 +0300

    true global

--- src/plugins-registry.js
+++ src/plugins-registry.js
@@ -1,46 +1,65 @@
 
 // # resigtry state
-const _pluginsApps = {}
-const __pluginsAppsOrd = []
-let _containerApp = null
+if (!global._k_app_plugins_plugins_state) {
+    global._k_app_plugins_plugins_state = {
+        apps: {},
+        ord: []
+    }
+}
+
+function _getPluginApps() {
+    return global._k_app_plugins_plugins_state.apps
+}
+
+function _getPluginAppsOrd() {
+    return global._k_app_plugins_plugins_state.ord
+}
+
+function _getApp() {
+    return global._k_app_plugins_containerApp
+}
+
+function _setApp(app) {
+    global._k_app_plugins_containerApp = app
+}
 
 // # plugin side - regitration
 function registerPluginApp(pluginKey, componentProvider, metadata) {
     const pluginRegistryItem = {componentProvider, metadata}
-    _pluginsApps[pluginKey] = pluginRegistryItem
-    __pluginsAppsOrd.push(pluginKey)
+    _getPluginApps()[pluginKey] = pluginRegistryItem
+    _getPluginAppsOrd().push(pluginKey)
 
     return pluginRegistryItem
 }
 
 // # app side - querying
 function getPluginAppKeys() {
-    return __pluginsAppsOrd
+    return _getPluginAppsOrd()
 }
 
 function getPluginApp(pluginKey) {
-    return _pluginsApps[pluginKey]
+    return _getPluginApps()[pluginKey]
 }
 
 // # app side - registration
 function registerContainerApp(containerApp) {
-    _containerApp = containerApp
+    _setApp(containerApp)
     return containerApp
 } 
 
 // # app side/plugin side - running, exiting
 function runPluginApp(pluginKey, parameters) {
-    if (!_containerApp)
+    if (!_getApp())
         throw new Error("runPluginApp: no container app available")
     
-    return _containerApp.runPluginApp(pluginKey, parameters)
+    return _getApp().runPluginApp(pluginKey, parameters)
 }
 
 function exitPluginApp(pluginKey) {
-    if (!_containerApp)
+    if (!_getApp())
         throw new Error("exitPluginApp: no container app available")
     
-    return _containerApp.exitPluginApp(pluginKey)
+    return _getApp().exitPluginApp(pluginKey)
 }
 
 module.exports = {
